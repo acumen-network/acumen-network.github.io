a7a4080d2a8c3cb2c664166d146261ca343bb455
ekougs <ekougs@gmail.com>
2017-09-06 16:42:11 +0000

    Fixed header for better fullpage sections display

--- wp-content/themes/bridge/js/default_dynamic9045.js
+++ wp-content/themes/bridge/js/default_dynamic9045.js
@@ -18,6 +18,7 @@ function ajaxSubmitCommentForm(){
 }
 var header_height = 100;
 var min_header_height_scroll = 57;
+var min_header_height_fixed_hidden = 50;
 var min_header_height_sticky = 60;
 var scroll_amount_for_sticky = 85;
 var content_line_height = 60;
@@ -25,19 +26,16 @@ var header_bottom_border_weight = 1;
 var scroll_amount_for_fixed_hiding = 200;
 var paspartu_width_init = 0.02;
 var add_for_admin_bar = 0;
-header_height = 110;
+header_height = 90;
 min_header_height_sticky = 80;
-scroll_amount_for_sticky = 340;
+scroll_amount_for_sticky = 15000;
 
 
-var logo_height = 130; // proya logo height
+var logo_height = 70; // proya logo height
 var logo_width = 280; // proya logo width
-logo_height = 220;
-logo_width = 428;
 
 header_top_height = 0;
 
-			header_top_height = 33;
 var loading_text;
 loading_text = 'Loading new posts...';
 var finished_text;
